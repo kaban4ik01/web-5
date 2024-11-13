6dcfb8cfb14646833bf7e4fc0799d49b7a267c17
kaban4ik01 <157748001+kaban4ik01@users.noreply.github.com>
2024-11-13 08:08:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -29,5 +29,5 @@ document.getElementById('button').addEventListener('click', function onClick(eve
 window.addEventListener('DOMContentLoaded', function (event) {
     console.log("DOM fully loaded and parsed");
     let b = document.getElementById("button");
-    b.addEventListener("click", onClick);
+    b.addEventListener("click", onClick());
   });
